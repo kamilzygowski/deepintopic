0c61b608fc85c75db34bee346cc41fcd967a9ef9
swedishsailor <kzygowski7@gmail.com>
2021-11-19 14:04:24 +0100

    before redux

--- src/components/MainView/MainView.js
+++ src/components/MainView/MainView.js
@@ -5,7 +5,8 @@ import './MainView.scss';
 class MainView extends React.Component {
     render() {
         return (
-            <section className="mainview" id="/mainview">    
+            <section className="mainview" id="/mainview">   
+                <h2 className="mainviewTitle"> title </h2> 
                <p> xd</p>
             </section>
         );
