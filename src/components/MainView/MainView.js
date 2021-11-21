f8d2ca47a11ce4c03a7cc7f81b571c6bf9c7eeba
swedishsailor <kzygowski7@gmail.com>
2021-11-21 13:58:50 +0100

    main aerticle view display

--- src/components/MainView/MainView.js
+++ src/components/MainView/MainView.js
@@ -7,12 +7,21 @@ function MainView () {
 
         const article = useSelector(state => state.articleReducer);
         return (
-            <section className="mainview" id="/mainview">   
+            <section className="mainview" id="/mainview">  
+            
+            <div className="inner-border">
+            <img class="vertical-decoration top" src="https://i.ibb.co/JRTK9z4/horizontally-centered-vertical-decoration.png"></img> 
+
                 <h2 className="mainviewTitle"> {article.title} </h2> 
                 <img className="arrows" src="https://i.postimg.cc/xjXZ6hPP/Untitled31.png"/>
                 <p className="articleText"> {article.text}</p>       
                 <p className="articleText"> {article.text2}</p>
                 <img className="arrows" src="https://i.postimg.cc/xjXZ6hPP/Untitled31.png"/>
+                <br/>
+                <br/>
+                <img class="vertical-decoration bottom" src="https://i.ibb.co/JRTK9z4/horizontally-centered-vertical-decoration.png"></img>
+                </div>
+                
             </section>
         );
     }
