8602f4fea46611175d6e72706dcba53935b2b619
swedishsailor <kzygowski7@gmail.com>
2021-12-03 20:35:43 +0100

    images can be added

--- src/components/MainView/MainView.js
+++ src/components/MainView/MainView.js
@@ -11,7 +11,7 @@ function MainView () {
             
             <div className="inner-border">
             <img className="vertical-decoration top" src="https://i.ibb.co/JRTK9z4/horizontally-centered-vertical-decoration.png" alt="deco"></img> 
-
+                {/*{article.titleImg ? <img className="articleTitleImg" alt=" " src={article.titleImg}/> : null}*/}
                 <h2 className="mainviewTitle"> {article.title} </h2> 
                 <img className="arrows" src="https://i.postimg.cc/xjXZ6hPP/Untitled31.png" alt="arrow"/>
                 {article.text ? <p className="articleText"> {article.text}</p> : null}
@@ -19,6 +19,7 @@ function MainView () {
                 {article.text2 ? <p className="articleText"> {article.text2}</p> : null}
                 {article.img2 ? <img className="articleImg" alt=" " src={article.img2}/> : null}     
                 {article.text3 ? <p className="articleText"> {article.text3}</p> : null}
+                {article.img3 ? <img className="articleImg" alt=" " src={article.img3}/> : null}  
                 <br/>
                 <img className="arrows" src="https://i.postimg.cc/xjXZ6hPP/Untitled31.png" alt="arrow"/>
                 <br/>
--- src/data/articles.js
+++ src/data/articles.js
@@ -2,58 +2,73 @@ export const articles = {
     articles: [
         {
             title: `Witam u mnie, siadaj, wstaw popcorn i czytaj`,
-            text: `No co klikaj w artykuł jak chcesz coś przeczytać, jak nie to nauuura`,
+            text: `Trafiłeś/aś na pseudo-naukowy blog, gdzie znajdziesz różne głupoty, ale i czasem coś ciekawego`,
             text2:``,
-            img1: ``,
+            text3:``,
+            img1: `https://i.postimg.cc/fT7y7j8J/IMG-7413.png`,
             img2: ``,
+            img3:``,
             id: 0
         },
         {
             title: `Zbawczy oddech (hiperwentylacja)`,
             text: `Dla zilustrowania, czując się w takim stanie, że już kompletnie odpadasz i czujesz się źle czy to nerwowo, psychicznie, czy to kręci ci się w głowie fizycznie. Zalecam użyć sposobu skupienia się na jednym punkcie przed sobą i wtedy głęboki wdech, głęboki wydech, stopniowo... zwalniasz...`,
             text2: ` Ale kluczem w żadnym stopniu nie jest oddech, tylko nastawienie. Musimy sposobem podzialac trochę na naszą neurologie i przeskoczyć frustracje spowodowaną tym że oddech działa (bo na początku nie działa). Trwając w tym przekonaniu, że za moment przyjdzie do nas ukojenie musimy wyczekać... No co, ogólnie wiara w rzeczy i zjawiska jest wszechpotężna, radze każdemu mocno to wykorzystywać.`,
+            text3: ``,
             img1: ``,
             img2: ``,
+            img3:``,
             id: 1
         },
         {
             title: `Ukierunkowany myślenie tam gdzie chcemy`,
             text: `ło kurwe, jak ten artykuł wyjdzie to buzia będzie otwierana`,
             text2:``,
+            text3:``,
             img1: ``,
             img2: ``,
+            img3:``,
             id: 2
         },
         {
             title: `Podejście metafizyczne i egzystencjalne`,
             text: `Przede wszystkim zacznijmy trochę od tematu artykułu. Uściślam się w nim do dwóch sposbów myślenia, tak zwanych 'mindsetów', są to: mindset metafizyczny, gdzie poczucie naszego bytu lokuje się gdzieś na poziomie - niewazne co się stanie i tak będę jednolitym bytem duchowym nawet jak umrę. Drugie podejście jest zaś 'egzystencjalnym' - gdzie bardziej kładziemy nacisk na tu i teraz, oraz na swoje wszystkie objawy somatyczne (boimy się śmierci)`,
             text2:`Podstawowa różnica między tymi podejściami rozbija się najczęściej o ich fundamenty. Podejście egzystencjalne zbudowane jest na strachu i ostrożności (realizm), zaś metafizyczne na pewności siebie/kreatywności i chęci poznania, oraz na (często tymczasowym) braku zainteresowania fizycznym życiem doczesnym.`,
+            text3: ``,
             img1: ``,
             img2: ``,
+            img3:``,
             id: 3
         },
         {
             title: `Omori - PTSD/wyparcie`,
-            text: `Krótkie intro: Omori jest to gra, w której doświadczamy świat jako osoba (jak się wydaje na pierwszy rzut oka) z depresją czy podobnym zaburzeniem. Jako bohater mamy 2 osobowości - prawdziwą i ochronną. Cała akcja rozgrywa się na przemian w świecie prawdziwym, jak i w tak zwanym 'white spacie', które jest swego rodzaju kaftanem ochronnym na mózg, gdzie bohater konserwuje swoje myśli, pragnienia, dzieciństwo, traumy itd. Wszystko to współgra w akompaniamencie cudnej, klimatycznej muzyki, oraz przeplatanką abstrakcyjnych i smutnych scen.`,
+            titleImg:`https://i.postimg.cc/3xk96Rz5/capsule-616x353.jpg`,
+            text: `Krótkie intro: Omori `+<span className="dictionaryWord">jest</span>+` to gra, w której doświadczamy świat jako osoba (jak się wydaje na pierwszy rzut oka) z depresją czy podobnym zaburzeniem. Jako bohater mamy 2 osobowości - prawdziwą i ochronną. Cała akcja rozgrywa się na przemian w świecie prawdziwym, jak i w tak zwanym 'white spacie', które jest swego rodzaju kaftanem ochronnym na mózg, gdzie bohater konserwuje swoje myśli, pragnienia, dzieciństwo, traumy itd. Wszystko to współgra w akompaniamencie cudnej, klimatycznej muzyki, oraz przeplatanką abstrakcyjnych i smutnych scen.`,
             text2:` Cała ta składanka tworzy piękny obraz życia pogrążonego w schizofrenicznej samotności i strachu. Gdzie przeżywamy dziecięce historie, które z razu na raz wchodzą w coraz większą psychodelę i na coraz "doroślejsze" tory. Ale to tylko tło, niestety i na szczęście. Głównym zadaniem tytułowego Omoriego, jest znalezienie swojego kochanego przyjaciela Basila, którego po krótkim spotkaniu na początku nikt nie może znaleźć. `,
+            text3:``,
             img1: 'https://i.postimg.cc/Y0tvfzSM/Omori-Hero500.png',
             img2: 'https://i.postimg.cc/sfKxCSXG/unknown.png',
+            img3:``,
             id: 4
         },
         {
             title:`Sprawy wymagające mózgu`,
             text:`Zacznijmy od poszczególnych pól w mózgu, które będą tutaj najważniejsze. `,
             text2:``,
+            text3:``,
             img1: ``,
             img2: ``,
+            img3:``,
             id:5
         },
         {
             title:`Żyj długo i twórczo`,
             text:`Taki tytuł ma książka jednego z moich ulubionych neurologów, a mianowicie profesora Jerzego Vetulaniego. Ale ja tutaj nie o tym. Dziś porozmawiamy sobie torszkę o pasji, no i troszkę też o motywacji.`,
             text2:`Będąc niczym w końcu po czasie, zostajemy zesłani tutaj, na ziemię. Budzimy się, nie kumamy nic oprócz "czegoś" co nas dziwnie irytuje ale i intryguje. Tak, chodzi o otoczenie, powietrze, dźwięk, zapach, po prostu każdy nasz niewykształcony zmysł szaleje. Teraz żmudne lata nauki, tak idzie, rok, drugi. Będąc ciągle gdzieś zawieszonym między powinnością, a niewiedzą błądzimy starając się z całych sił pojąc ten zewnętrzny byt. Czasem jest fajnie, cieszymy się chwilą, czasem jest dziwnie i tak chłodno w brzuchu. Nie wiem, każdy inaczej to przeżywa, ale no powiedzmy, że bywa różnie.  `,
-            img1: ``,
+            text3:``,
+            img1: `https://i.postimg.cc/6Tx7WMYN/2c598adfb2a850b71c2eef85efb933ec.jpg`,
             img2: ``,
+            img3:``,
             id:6
         }
     ]
